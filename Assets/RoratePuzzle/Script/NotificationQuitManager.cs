bbf421840ebdb18c5cd8fcf2c6a54d1823bd54fc
TUENGUYEN-190055 <190055ej@yse-c.net>
2020-12-25 14:37:48 +0900

    1225_save

---
+++ Assets/RoratePuzzle/Script/NotificationQuitManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotificationQuitManager : MonoBehaviour
+{
+    public Animator animator;
+    public MainManager mainManager;
+    public Timer timer;
+    public GameManager gameManager;
+    // Start is called before the first frame update
+    
+
+
+    public void Open()
+    {
+        animator.SetBool("IsOpen", true);
+    }
+
+    public void Close()
+    {
+        animator.SetBool("IsOpen", false);
+
+    }
+
+    public void BtnYes()
+    {
+        Close();
+    }
+}
