f673dd5237138bbec5c01e2edd7bc0b00880e0a3
190256 <190256hr@yse-c.net>
2020-12-25 10:43:46 +0900

    tsumuのプレハブ修正Ver.2

---
+++ Assets/TsumuAssets/Scripts/ButtonManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour {
+    public void click() {
+        GetComponent<Button>().interactable = false;
+    }
+}
