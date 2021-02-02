a35797ec876508fe77e831fbd905156dd63bca42
190256 <190256hr@yse-c.net>
2021-02-02 13:43:30 +0900

    終了ボタン修正

--- Assets/Script/GameQuit.cs
+++ Assets/Script/GameQuit.cs
@@ -6,8 +6,13 @@ using System;
 public class GameQuit : MonoBehaviour {
 
     public void GameQuitClick() {
-        UnityEditor.EditorApplication.isPlaying = false;
-        UnityEngine.Application.Quit();
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_WEBPLAYER
+            Application.OpenURL("http://www.yahoo.co.jp/");
+        #else
+            Application.Quit();
+        #endif
     }
 
 }
