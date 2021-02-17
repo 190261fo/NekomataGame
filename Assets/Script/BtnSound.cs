11e515711b4bbf4a78b5313ea06d827332027883
190256 <190256hr@yse-c.net>
2021-02-17 12:15:26 +0900

    SE追加

--- Assets/Script/BtnSound.cs
+++ Assets/Script/BtnSound.cs
@@ -7,5 +7,21 @@ public class BtnSound : MonoBehaviour {
     public void Click() {
         AudioManager.GetInstance().PlaySound(7);
     }
+
+    public void Save_Click() {
+        AudioManager.GetInstance().PlaySound(8);
+    }
+
+    public void Load_Click() {
+        AudioManager.GetInstance().PlaySound(9);
+    }
     
+    public void Cancel_Click() {
+        AudioManager.GetInstance().PlaySound(15);
+    }
+
+    public void PointerEnter_Btn() {
+        AudioManager.GetInstance().PlaySound(16);
+    }
+
 }
